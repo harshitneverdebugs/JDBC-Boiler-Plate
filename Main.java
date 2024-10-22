ed082616fe43b2388b655a930028535154f46d42
Harshit Singh <139043409+harshitneverdebugs@users.noreply.github.com>
2024-10-22 11:16:49 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.sql.*;
 public class Main {
-    private static final String url="jdbc:mysql://localhost:3306/?user=root";
+    private static final String url="jdbc:mysql://localhost:3306/?user=root";  //replace ?user=root with the name of the databse currently being used
     private static final String username="root";
     private static final String password="wj28@krhps";
     public static void main(String[] args) {
